1e0fe05719017ae04586063c79c02e0eff9b0c54
Edgars Joja <edgarsjoja@gmail.com>
2022-10-21 23:28:42 +0300

    Update SHA sidebar naming

--- src/layout/SideBar.svelte
+++ src/layout/SideBar.svelte
@@ -15,7 +15,7 @@
 
     const apps: App[] = [
         { appCode: 'json_viewer', label: 'Json Viewer' },
-        { appCode: 'sha', label: 'Sha-X Hash' },
+        { appCode: 'sha', label: 'SHA Hash' },
         { appCode: 'notepad', label: 'Notepad' },
         { appCode: 'whiteboard', label: 'Whiteboard' },
         { appCode: 'jwt', label: 'JWT' },
